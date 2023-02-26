74c7dbc8c871906d3578059f74af7cfdfe532eea
Induranga-kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2023-02-26 15:58:37 +0530

    Added_Edit_Profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart';
-import 'camera_page.dart';
+import 'package:skin_detection/edit_profile.dart';
+
 void main() {
   runApp(const App());
 }
@@ -12,7 +12,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:LoginPage(),
+      home: edit_profile(),
     );
   }
 }
