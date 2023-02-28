195e4154ca26846bf3face12100b1d9ede19088b
bk_viper <90303435+buKaushalya@users.noreply.github.com>
2023-02-28 16:10:06 +0530

    select from gallery
    created select from gallery part

---
+++ lib/image_helper.dart
@@ -0,0 +1,30 @@
+import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
+class ImageHelper{
+  ImageHelper({
+    ImagePicker? imagePicker,
+    ImageCropper? imageCropper,
+  }) : _imagePicker = imagePicker ?? ImagePicker(),
+       _imageCropper = imageCropper ?? ImageCropper();
+
+  final ImagePicker _imagePicker;
+  final ImageCropper _imageCropper;
+
+  pickImage({
+    ImageSource source = ImageSource.gallery,
+    int imageQuality = 100,
+  }) async{
+    return await _imagePicker.pickImage(
+        source: source,
+        imageQuality: imageQuality,
+        );
+  }
+
+  Future<CroppedFile?> crop({
+    required XFile file,
+    CropStyle cropStyle = CropStyle.rectangle,
+  }) async => 
+      await _imageCropper.cropImage(
+          cropStyle: cropStyle,
+          sourcePath: file.path,);
+}
