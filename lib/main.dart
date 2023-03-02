a9ed09101cb47121bd451f696dff329974cecd05
bk_viper <90303435+buKaushalya@users.noreply.github.com>
2023-03-03 02:18:43 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: edit_profile(),
+      home: LoginPage(),
     );
   }
 }
