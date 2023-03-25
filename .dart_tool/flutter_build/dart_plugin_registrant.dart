fff083c87d9c9086511744b095da5ac46af90175
bk_viper <90303435+buKaushalya@users.noreply.github.com>
2023-03-25 18:23:28 +0530

    Edited some changes

--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -7,7 +7,9 @@
 
 import 'dart:io'; // flutter_ignore: dart_io_import.
 import 'package:camera_android/camera_android.dart';
+import 'package:image_picker_android/image_picker_android.dart';
 import 'package:camera_avfoundation/camera_avfoundation.dart';
+import 'package:image_picker_ios/image_picker_ios.dart';
 
 @pragma('vm:entry-point')
 class _PluginRegistrant {
@@ -25,12 +27,32 @@ class _PluginRegistrant {
         rethrow;
       }
 
+      try {
+        ImagePickerAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`image_picker_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
     } else if (Platform.isIOS) {
       try {
         AVFoundationCamera.registerWith();
       } catch (err) {
         print(
           '`camera_avfoundation` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+      try {
+        ImagePickerIOS.registerWith();
+      } catch (err) {
+        print(
+          '`image_picker_ios` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
         rethrow;
