79e33fd5c314cdb83616639947ad287dee96a005
Abdullah Sanoosi <abdullahsanoosi100@gmail.com>
2023-04-05 20:50:32 +0530

    Saved CNN model in .h5 file format

--- lib/doctorClass.dart
+++ lib/doctorClass.dart
@@ -3,11 +3,10 @@ class Doctor{
   String? adress;
   String? title;
   String? email;
-  String? degree;
   String? image;
   List hospitals;
 
-  Doctor(this.name, this.adress, this.title, this.email, this.degree,this.image,
+  Doctor(this.name, this.adress, this.title, this.email, this.image,
       this.hospitals);
 
 }
